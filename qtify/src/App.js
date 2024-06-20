33329a52ec824c1b6339c920414859caa6409ba7
CodesOfVysakh <onvysakh309@gmail.com>
2024-06-20 23:48:09 +0530

    applied all in one :app

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,16 +1,27 @@
 import logo from "./components/assets/images/qtify-logo.svg";
 import "./App.css";
 import Button from "./components/Button/Button";
-import SearchBar from "./components/SearchBar/SearchBar";
+import styles from './components/SearchBar/Search.module.css';
+import SearchImage from './components/assets/images/Search-icon.svg'
+import buttonstyle from './components/Button/Button.module.css'
+
+// import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
     return (
         <div className="App">
-            <nav className="navbarContainer">
+            <header className="navbarContainer App-header">
                 <img src={logo} alt="Logo" />
-                <SearchBar />
-                <Button>Give Feedback</Button>
-            </nav>
+                {/* <SearchBar /> */}
+				<div className={styles.searchbar}>
+					<input 
+						type="text" 
+						placeholder='Search an album of your choice'
+					/>
+					<img src={SearchImage} alt="Search Icon" />
+				</div>
+                <button className={buttonstyle.button}>Give Feedback</button>
+            </header>
         </div>
     );
 }
