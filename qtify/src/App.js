f428d189996f2e85081659d122497efe860974a2
CodesOfVysakh <onvysakh309@gmail.com>
2024-06-20 22:56:53 +0530

    logo applied

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button/Button';
-import SearchBar from './components/SearchBar/SearchBar';
+import logo from "./components/assets/images/qtify-logo.svg";
+import "./App.css";
+import Button from "./components/Button/Button";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SearchBar />
-        <Button>Give Feedback</Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} alt="Logo" />
+                <SearchBar />
+                <Button>Give Feedback</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
