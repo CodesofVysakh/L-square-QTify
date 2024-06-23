7febf50aadb4a976999f5fe3a4c212638b152e97
CodesOfVysakh <onvysakh309@gmail.com>
2024-06-23 12:28:56 +0530

    Hero section completed

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,4 +1,5 @@
 import logo from "./components/assets/images/qtify-logo.svg";
+import HeadPhone from "./components/assets/images/vibrating-headphone.svg";
 import "./App.css";
 import Button from "./components/Button/Button";
 import styles from './components/SearchBar/Search.module.css';
@@ -13,15 +14,22 @@ function App() {
             <header className="navbarContainer App-header">
                 <img src={logo} alt="Logo" />
                 {/* <SearchBar /> */}
-				<div className={styles.searchbar}>
+				<div className={"searchbar"}>
 					<input 
 						type="text" 
 						placeholder='Search an album of your choice'
 					/>
 					<img src={SearchImage} alt="Search Icon" />
 				</div>
-                <button className={buttonstyle.button}>Give Feedback</button>
+                <button className={"button"}>Give Feedback</button>
             </header>
+			<div className="Hero-container">
+				<div className="Hero-content">
+					<h3 className="Hero-title">100 Thousand Songs, ad-free</h3>
+					<h3 className="Hero-title">Over thousands podcast episodes</h3>
+				</div>
+                <img src={HeadPhone} alt="Hero Image" />
+			</div>
         </div>
     );
 }
