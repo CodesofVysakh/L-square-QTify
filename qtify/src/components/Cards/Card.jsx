5e37b1be9871cb95be739cbfb129d58798b377f2
CodesOfVysakh <onvysakh309@gmail.com>
2024-07-30 20:18:04 +0530

    tooltip added for cards

--- qtify/src/components/Cards/Card.jsx
+++ qtify/src/components/Cards/Card.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import styles from './Card.module.css'
-import { Chip } from '@mui/material';
+import React from "react";
+import styles from "./Card.module.css";
+import { Chip, Tooltip } from "@mui/material";
 
 function Card({ data, type }) {
-  return (
-    <div className={styles.MainContainer}>
-        <div className={styles.Card}>
-            <div className={styles.Image}>
-                <img src={data?.image} alt="Album Image" />
+    return (
+        <Tooltip title={data.songs && `${data?.songs?.length} songs`} placement="top">
+            <div className={styles.MainContainer}>
+                <div className={styles.Card}>
+                    <div className={styles.Image}>
+                        <img src={data?.image} alt="Album Image" />
+                    </div>
+                    <div className={styles.Follow}>
+                        <Chip
+                            label={
+                                type == "album"
+                                    ? `${data?.follows} Follows`
+                                    : `${data?.likes} Likes`
+                            }
+                            sx={{ backgroundColor: "#000", color: "#fff" }}
+                        />
+                    </div>
+                </div>
+                <div>{data?.title}</div>
             </div>
-            <div className={styles.Follow}>
-                <Chip label={type == "album" ? `${data?.follows} Follows` : `${data?.likes} Likes`} sx={{ backgroundColor: "#000", color: "#fff"}}/>
-            </div>
-        </div>
-        <div>
-            {data?.title}
-        </div>
-    </div>
-  )
+        </Tooltip>
+    );
 }
 
-export default Card
+export default Card;
